1b16e5ac23b1d4fd14d7b4a65b3af958d94c1e5b
mickmister <mjkochell@gmail.com>
2019-08-16 02:54:14 -0400

    app just renders a dummy mobile page

--- src/app.tsx
+++ src/app.tsx
@@ -14,23 +14,31 @@ import SongSplicerPage from './pages/song-splicer/song-splicer-page'
 import ShowSongPage from './pages/show-song/show-song-page'
 import Navbar from './components/navbar/navbar';
 import Footer from './components/footer/footer';
+import Mobile from './components/mobile/mobile'
 
 const root = document.getElementById('main')
 
-ReactDOM.render(
-  // @ts-ignore
-  <BrowserRouter>
+ReactDOM.render(<Mobile />, root)
+
+const r = () => {
+
+
+  ReactDOM.render(
+    // @ts-ignore
+    <BrowserRouter>
     <StoreProvider store={store}>
-      <StoreInit>
-        <Navbar />
-        <Route exact path="/" component={() => <Redirect exact from="/" to="/songs" />} />
-        <Route path="/login" exact component={LoginPage} />
-        <Route path="/songs" exact component={ChooseSongPage} />
-        <Route path="/songs/:id/splice" component={SongSplicerPage} />
-        <Route path="/songs/:id/play" component={ShowSongPage} />
-        <Footer />
-      </StoreInit>
+    <StoreInit>
+    <Navbar />
+    <Route exact path="/" component={() => <Redirect exact from="/" to="/songs" />} />
+    <Route path="/login" exact component={LoginPage} />
+    <Route path="/songs" exact component={ChooseSongPage} />
+    <Route path="/songs/:id/splice" component={SongSplicerPage} />
+    <Route path="/songs/:id/play" component={ShowSongPage} />
+    <Footer />
+    </StoreInit>
     </StoreProvider>
-  </BrowserRouter>,
-  root,
-)
+    </BrowserRouter>,
+    root,
+    )
+
+  }
